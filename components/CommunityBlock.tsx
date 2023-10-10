b35c6aac2fe7dab702d7bbc202019bc733f206d3
Manav Babber <manavfazilka@gmail.com>
2023-10-10 16:45:11 +0530

    feat: added dropdown on home page

--- components/CommunityBlock.tsx
+++ components/CommunityBlock.tsx
@@ -1,23 +1,23 @@
 import Image from 'next/image';
 function CommunityBlock({community}: any) {
     return (
-        <div className="relative w-64 h-64">
+        <div className="relative hover:cursor-pointer">
             <svg xmlns="http://www.w3.org/2000/svg" width="241" height="165" viewBox="0 0 241 165" fill="none">
-                <path d="M3.28516 65.574V44.3898L32.3782 3.15112H209.479L237.442 44.3898V65.574" stroke="#A55FFD" stroke-width="5.31252"/>
-                <path d="M3.28534 99.7782V120.962L32.3784 162.201H209.479L237.442 120.962V99.7782" stroke="#A55FFD" stroke-width="5.31252"/>
+                <path d="M3.28516 65.574V44.3898L32.3782 3.15112H209.479L237.442 44.3898V65.574" stroke="#A55FFD" strokeWidth="5.31252"/>
+                <path d="M3.28534 99.7782V120.962L32.3784 162.201H209.479L237.442 120.962V99.7782" stroke="#A55FFD" strokeWidth="5.31252"/>
                 <g filter="url(#filter0_b_496_65)">
-                    <path d="M35.7677 10.7774L10.0644 48.0617V115.569L35.7677 153.983H206.089L231.793 115.569V48.0617L206.089 10.7774H35.7677Z" stroke="#2B37E6" stroke-width="1.32813"/>
+                    <path d="M35.7677 10.7774L10.0644 48.0617V115.569L35.7677 153.983H206.089L231.793 115.569V48.0617L206.089 10.7774H35.7677Z" stroke="#2B37E6" strokeWidth="1.32813"/>
                 </g>
                 <defs>
-                    <filter id="filter0_b_496_65" x="0.226996" y="0.939994" width="241.403" height="162.88" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                    <filter id="filter0_b_496_65" x="0.226996" y="0.939994" width="241.403" height="162.88" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix"/>
                     <feGaussianBlur in="BackgroundImageFix" stdDeviation="4.58667"/>
                     <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_496_65"/>
                     <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_496_65" result="shape"/>
                     </filter>
                 </defs>
             </svg>
-        <div className="absolute left-1/2 -mb-12 -ml-1 items-center flex flex-col bottom-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div className="absolute bottom-1/3 -mb-4 left-1/3 -ml-6 items-center flex flex-col ">
             <Image src='/Logo.png' width={50} height={50} alt="Community Logo"/>
             <p className='text-gray-100'>{community?.name}</p>
             <p className='text-gray-700'>{community?.members}</p>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
+import { useState, useRef, useEffect } from 'react';
 import CommunityBlock from '@/components/CommunityBlock';
+import Dropdown from '@/components/DropDown';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -55,12 +57,30 @@ export default function Home() {
   ];
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}
+      className={`min-h-screen items-center justify-center py-12 ${inter.className}`}
     >
-      <div className='flex gap-8 flex-wrap justify-center items-center'>
-        {list.map((listItem) => (
-          <CommunityBlock community={listItem}/>
-        ))}
+      <div className='flex mb-4 flex-row flex-wrap px-20 gap-4'>
+        <div className='flex gap-3 text-xs'>
+          <p className='text-gray-100 ring-1 ring-custom-purple border py-2 px-4 rounded-2xl'>COMMUNITIES 29.5K</p>
+          <p className='text-gray-100 ring-1 ring-custom-purple border py-2 px-4 rounded-2xl'>PROPOSALS 119.5K</p>
+        </div>
+        <div className='ml-auto flex gap-3 text-xs'>
+          <Dropdown/>
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 35 35" fill="none">
+              <path d="M22.333 21.0579C23.425 19.8537 24.0903 18.2555 24.0903 16.5019C24.0903 12.7549 21.0527 9.71732 17.3056 9.71732C13.5586 9.71732 10.521 12.7549 10.521 16.5019C10.521 20.249 13.5586 23.2866 17.3056 23.2866C19.2991 23.2866 21.0918 22.4268 22.333 21.0579ZM22.333 21.0579L25.871 24.4624" stroke="#A55FFD" strokeWidth="2" strokeLinecap="round"/>
+              <rect x="1.90186" y="1.06693" width="32" height="32" rx="16" stroke="#9455E4" strokeWidth="2"/>
+            </svg>
+          </span>
+        </div>
+      </div>
+      <hr className='mb-6 border-custom-purple mx-20'/>
+      <div className='flex z-10 gap-8 flex-wrap justify-center items-center'>
+        {
+          list.map((listItem) => (
+            <CommunityBlock key={listItem.name} community={listItem}/>
+          ))
+        }
       </div>
     </main>
   )
