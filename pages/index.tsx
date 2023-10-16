1684c74bda39c2a3efec0d6ad5c6c7507e6a4609
Manav Babber <manavfazilka@gmail.com>
2023-10-16 17:55:48 +0530

    feat: integrated getAllDaos api

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,65 +4,7 @@ import HomePageBar from '@/components/HomePageBar';
 import Link from 'next/link';
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
-  const list = [
-    {
-      id: '1',
-      logo: '/Logo.png',
-      name:'AAVE',
-      members: '127.3K MEMBERS',
-      link: '/aave'
-    },
-    {
-      id:'2',
-      logo: '/Logo.png',
-      name:'AAVE',
-      members: '127.3K MEMBERS',
-      link: '/aave'
-    },
-    {
-      id:'3',
-      logo: '/Logo.png',
-      name:'AAVE',
-      members: '127.3K MEMBERS',
-      link: '/aave'
-    },
-    {
-      id:'4',
-      logo: '/Logo.png',
-      name:'AAVE',
-      members: '127.3K MEMBERS',
-      link: '/aave'
-    },
-    {
-      id:'5',
-      logo: '/Logo.png',
-      name:'AAVE',
-      members: '127.3K MEMBERS',
-      link: '/aave'
-    },
-    {
-      id:'6',
-      logo: '/Logo.png',
-      name:'AAVE',
-      members: '127.3K MEMBERS',
-      link: '/aave'
-    },
-    {
-      id:'7',
-      logo: '/Logo.png',
-      name:'AAVE',
-      members: '127.3K MEMBERS',
-      link: '/aave'
-    },
-    {
-      id:'8',
-      logo: '/Logo.png',
-      name:'AAVE',
-      members: '127.3K MEMBERS',
-      link: '/aave'
-    }
-  ];
+export default function Home({daos}:any) {
   return (
     <main
       className={`min-h-screen items-center justify-center py-12 ${inter.className}`}
@@ -70,13 +12,23 @@ export default function Home() {
       <HomePageBar />
       <div className='flex z-10 gap-8 flex-wrap justify-center items-center'>
         {
-          list.map((listItem, idx) => (
+          daos.map((listItem: any, idx: number) => (
             <Link key={idx} href={`/community/${listItem.id}`}>
-              <CommunityBlock key={listItem.name} community={listItem}/>
+              <CommunityBlock key={listItem._id} community={listItem}/>
             </Link>
           ))
         }
       </div>
     </main>
   )
+}
+
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3001/api/getAllDaos');
+  const daos = await response.json();
+  return {
+    props: {
+      daos
+    }
+  };
 }
