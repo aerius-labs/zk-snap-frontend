b04943cc8ac1d34b1622c2b13b80073540685962
Manav Babber <manavfazilka@gmail.com>
2023-10-16 17:55:48 +0530

    feat: integrated getAllDaos api

---
+++ pages/api/getAllDaos.ts
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+// type Data = {
+//   name: string
+// }
+
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const response = await fetch('http://localhost:3000/dao');
+  const daos = await response.json();
+  res.status(200).json(daos);
+}
