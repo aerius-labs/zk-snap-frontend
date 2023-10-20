51a30c9f6872ef87c808979b30ae28e64d6c1340
Manav Babber <manavfazilka@gmail.com>
2023-10-20 16:37:38 +0530

    feat(wip): changed new proposal design

--- pages/community/[id]/new-proposal.tsx
+++ pages/community/[id]/new-proposal.tsx
@@ -46,42 +46,42 @@ export default function Proposal({id}:any) {
   
       const result = await response.json();
       console.log(result);
-      console.log({ title, description, startDate, endDate, startTime, endTime });
+
   };
 
   return (
     <div className='min-h-screen flex justify-center items-center'>
-        <form onSubmit={handleSubmit} className="flex flex-col w-full items-center max-w-xl mx-auto p-6 justify-center space-y-4 bg-gray-100 rounded-md shadow-md">
-            <h1 className='text-lg m-6 text-black'>Create a New Proposal</h1>
+        <form onSubmit={handleSubmit} className="flex border border-custom-purple flex-col w-full items-center max-w-xl mx-auto p-6 justify-center space-y-4 rounded-md shadow-md">
+            <h1 className='text-lg m-6 text-custom-purple'>Create a New Proposal</h1>
             <div className="w-full">
                 <input
-                type="text"
-                className="w-full p-2 text-black bg-white border rounded-md"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                placeholder="Title"
+                  type="text"
+                  className="w-full p-2 text-black bg-gray-300 rounded-md"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  placeholder="Title"
                 />
             </div>
             <div className="w-full">
                 <textarea
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                className="w-full p-2 text-black bg-white border rounded-md"
-                placeholder="Description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  className="w-full p-2 text-black bg-gray-300 rounded-md"
+                  placeholder="Description"
+                />
+            </div>
+            <div className="w-full">
+                <DatePicker
+                  selected={startDate} 
+                  onChange={(date: any) => setStartDate(date)} 
+                  className="w-full p-2 text-black bg-gray-300 rounded-md"
                 />
             </div>
             <div className="w-full">
                 <DatePicker 
-                selected={startDate} 
-                onChange={(date: any) => setStartDate(date)} 
-                className="w-full p-2 text-black bg-white border rounded-md"
-                />
-            </div>
-            <div className="w-full">
-                <DatePicker 
-                selected={endDate} 
-                onChange={(date: any) => setEndDate(date)} 
-                className="w-full p-2 text-black bg-white border rounded-md"
+                  selected={endDate} 
+                  onChange={(date: any) => setEndDate(date)} 
+                  className="p-2 text-black bg-gray-300 rounded-md"
                 />
             </div>
             <div className="w-full">
@@ -93,7 +93,7 @@ export default function Proposal({id}:any) {
                 />
             </div>
             <div className="w-full">
-                <Datetime 
+                <Datetime
                     dateFormat={false}
                     value={endTime} 
                     onChange={setEndTime}
@@ -101,7 +101,7 @@ export default function Proposal({id}:any) {
                 />
             </div>
             <div>
-                <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">Submit</button>
+                <button type="submit" className="px-4 py-2 border border-custom-purple text-white rounded-md hover:ring-2 hover:ring-custom-purple">Submit</button>
             </div>
         </form>
     </div>
